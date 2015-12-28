15c92f83b2a1b8066d496795304b69c33d2317e7
Thaminda Edirisooriya <tediris@stanford.edu>
2015-12-28 00:38:37 -0700

    cleaned up the old rigidbody stuff. Need to rework the whole physics engine

--- physics/RigidBody.java
+++ physics/RigidBody.java
@@ -17,7 +17,7 @@ public class RigidBody extends Component{
 	public RigidBody(Entity e) {
 		super(e);
 		e.body = this;
-		e.game.addBody(this);
+		//e.game.addBody(this);
 		position = new Vector2(e.transform.x, e.transform.y);
 		velocity = new Vector2();
 		invMass = 1f;
@@ -28,7 +28,7 @@ public class RigidBody extends Component{
 	public void update() {
 		if (!kinematic) {
 			// account for gravity
-			velocity.y += 1.0 * gravityScale;
+			//velocity.y += 1.0 * gravityScale;
 		}
 		position.x += velocity.x;
 		position.y += velocity.y;
@@ -36,56 +36,56 @@ public class RigidBody extends Component{
 		entity.transform.y = position.y;
 	}
 
-	public static void ResolveCollision(RigidBody a, RigidBody b) {
-		Vector2 rv = Vector2.add(a.velocity, b.velocity.neg());
-		Vector2 normal = new Vector2();
-		float penetration = GetCollisionNormal(a.bounds, b.bounds, normal);
-		//System.out.println("Collision normal: " + normal);
-		float velNormal = Vector2.dot(rv, normal);
-		// if (velNormal > 0) {
-		// 	// we are already moving away from the collision
-		// 	return;
-		// }
-		resolveOverlap(a, normal, penetration);
-		resolveOverlap(b, normal.neg(), penetration);
+	// public static void ResolveCollision(RigidBody a, RigidBody b) {
+	// 	Vector2 rv = Vector2.add(a.velocity, b.velocity.neg());
+	// 	Vector2 normal = new Vector2();
+	// 	float penetration = GetCollisionNormal(a.bounds, b.bounds, normal);
+	// 	//System.out.println("Collision normal: " + normal);
+	// 	float velNormal = Vector2.dot(rv, normal);
+	// 	// if (velNormal > 0) {
+	// 	// 	// we are already moving away from the collision
+	// 	// 	return;
+	// 	// }
+	// 	resolveOverlap(a, normal, penetration);
+	// 	resolveOverlap(b, normal.neg(), penetration);
+	//
+	// 	float e = Math.min(a.bounce, b.bounce);
+	//
+	// 	float j = -(1 + e) * velNormal;
+	// 	j = j / (a.invMass + b.invMass);
+	//
+	// 	Vector2 impulse = normal.scale(j);
+	// 	//if (!a.kinematic) a.velocity = Vector2.add(a.velocity, impulse.scale(a.invMass).neg());
+	// 	//if (!b.kinematic) b.velocity = Vector2.add(b.velocity, impulse.scale(b.invMass));
+	// }
 
-		float e = Math.min(a.bounce, b.bounce);
-
-		float j = -(1 + e) * velNormal;
-		j = j / (a.invMass + b.invMass);
-
-		Vector2 impulse = normal.scale(j);
-		//if (!a.kinematic) a.velocity = Vector2.add(a.velocity, impulse.scale(a.invMass).neg());
-		//if (!b.kinematic) b.velocity = Vector2.add(b.velocity, impulse.scale(b.invMass));
-	}
-
-	private static void resolveOverlap(RigidBody b, Vector2 dir, float overlap) {
-		if (b.kinematic) return;
-		b.position.x += dir.x * overlap;
-		b.position.y += dir.y * overlap;
-		if (b.velocity.x * dir.x < 0) b.velocity.x = 0;
-		if (b.velocity.y * dir.y < 0) b.velocity.y = 0;
-	}
-
-	private static float GetCollisionNormal(BoundingBox a, BoundingBox b, Vector2 result) {
-		Vector2 aExtent = new Vector2((a.max.x - a.min.x)/ 2, (a.max.y - a.min.y) / 2);
-		Vector2 bExtent = new Vector2((b.max.x - b.min.x)/ 2, (b.max.y - b.min.y) / 2);
-		Vector2 n = Vector2.add(b.mid, b.body.position, a.mid.neg(), a.body.position.neg());
-		Vector2 overlap = Vector2.add(aExtent, bExtent, n.abs().neg());
-		if (overlap.x > overlap.y) {
-			if (n.y > 0) {
-				result.set(0, -1);
-			} else {
-				result.set(0, 1);
-			}
-			return overlap.y;
-		} else {
-			if (n.x > 0) {
-				result.set(-1, 0);
-			} else {
-				result.set(1, 0);
-			}
-			return overlap.x;
-		}
-	}
+	// private static void resolveOverlap(RigidBody b, Vector2 dir, float overlap) {
+	// 	if (b.kinematic) return;
+	// 	b.position.x += dir.x * overlap;
+	// 	b.position.y += dir.y * overlap;
+	// 	if (b.velocity.x * dir.x < 0) b.velocity.x = 0;
+	// 	if (b.velocity.y * dir.y < 0) b.velocity.y = 0;
+	// }
+	//
+	// private static float GetCollisionNormal(BoundingBox a, BoundingBox b, Vector2 result) {
+	// 	Vector2 aExtent = new Vector2((a.max.x - a.min.x)/ 2, (a.max.y - a.min.y) / 2);
+	// 	Vector2 bExtent = new Vector2((b.max.x - b.min.x)/ 2, (b.max.y - b.min.y) / 2);
+	// 	Vector2 n = Vector2.add(b.mid, b.body.position, a.mid.neg(), a.body.position.neg());
+	// 	Vector2 overlap = Vector2.add(aExtent, bExtent, n.abs().neg());
+	// 	if (overlap.x > overlap.y) {
+	// 		if (n.y > 0) {
+	// 			result.set(0, -1);
+	// 		} else {
+	// 			result.set(0, 1);
+	// 		}
+	// 		return overlap.y;
+	// 	} else {
+	// 		if (n.x > 0) {
+	// 			result.set(-1, 0);
+	// 		} else {
+	// 			result.set(1, 0);
+	// 		}
+	// 		return overlap.x;
+	// 	}
+	// }
 }
